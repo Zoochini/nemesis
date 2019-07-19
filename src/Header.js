bee0ee6434c9556ebe134a2bdc36fc28a439dde2
Zoochini <maxime.bardiot@gmail.com>
2019-07-19 18:05:41 +0200

    maj header

--- src/Header.js
+++ src/Header.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import { Nav, Navbar } from 'react-bootstrap';
+import { Nav, Navbar, Image, Col, Container, Row } from 'react-bootstrap';
 
 
 function Header() {
     return (
-        <Navbar bg="light" variant="light">
+        <Navbar bg="light" variant="dark" className="justify-content-center">
             <Navbar.Brand>
-                <Link to="/">Accueil</Link>
+                <Link to="/"><Image id="button-logo" src="/nemesis-logo.svg" alt="Accueil" width="32" height="32"></Image></Link>
             </Navbar.Brand>
-            <Nav justify>
+            <Nav>
                 <Nav.Link>
-                    <Link to="/stream">Stream</Link>
+                    <Link to="/stream" className="text-danger">Stream</Link>
                 </Nav.Link>
                 <Nav.Link>
-                    <Link to="/contact">Contact</Link>
+                    <Link to="/contact" className="text-danger">Contact</Link>
                 </Nav.Link>
             </Nav>
         </Navbar>
