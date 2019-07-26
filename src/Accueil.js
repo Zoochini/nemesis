bbc014229437fa2f8c6e62be8fdf6c52364c38bd
Zoochini <maxime.bardiot@gmail.com>
2019-07-27 01:07:45 +0200

    Refonte text + footer maj

--- src/Accueil.js
+++ src/Accueil.js
@@ -1,41 +1,60 @@
 import React from 'react';
 import { Jumbotron, Container, Row, Col, Carousel, Image } from 'react-bootstrap';
 
-function Accueil() {
-    const widthCarousel = window.innerWidth;
-    const heightCarousel = window.innerHeight;
+class Accueil extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            widthCarousel: window.innerWidth,
+            heightCarousel: window.innerHeight
+        }
+    }
 
-    return (
-        <div>
-            <Container fluid>
-                <Row className="justify-content-center">
-                    <Carousel>
-                        <Carousel.Item>
-                            <Image src="/nemesis-logo.svg" alt="First Slide" width={widthCarousel} height={heightCarousel}></Image>
+    componentDidMount(){
+        this.timerID = setInterval(
+            () => this.updateCarouselSize(),
+            10
+        );
+    }
+
+    updateCarouselSize(){
+        this.setState({widthCarousel : window.innerWidth});
+        this.setState({heightCarousel : window.innerHeight});
+    }
+
+    render() {
+        return (
+            <div>
+                <Container fluid>
+                    <Row className="justify-content-center">
+                        <Carousel>
+                            <Carousel.Item>
+                                <Image src="/nemesis-ban.svg" alt="First Slide" width={this.state.widthCarousel} height={this.state.heightCarousel}></Image>
+                                <Carousel.Caption>
+                                    <h3>Bienvenue</h3>
+                                    <p>Au coeur des petits four</p>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <Image src="/nemesis-logo.svg" alt="Second Slide" width={this.state.widthCarousel} height={this.state.heightCarousel}></Image>S
                             <Carousel.Caption>
-                                <h3>First slide label</h3>
-                                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                            </Carousel.Caption>
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <Image src="/nemesis-logo.svg" alt="Second Slide" width={widthCarousel} height={heightCarousel}></Image>S
-                            <Carousel.Caption>
-                                <h3>Second slide label</h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            </Carousel.Caption>
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <Image src="/nemesis-logo.svg" alt="Third Slide" width={widthCarousel} height={heightCarousel}></Image>
-                            <Carousel.Caption>
-                                <h3>Third slide label</h3>
-                                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                            </Carousel.Caption>
-                        </Carousel.Item>
-                    </Carousel>
-                </Row>
-            </Container>
-        </ div>
-    )
+                                    <h3>Second slide label</h3>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <Image src="/nemesis-logo.svg" alt="Third Slide" width={this.state.widthCarousel} height={this.state.heightCarousel}></Image>
+                                <Carousel.Caption>
+                                    <h3>Third slide label</h3>
+                                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                        </Carousel>
+                    </Row>
+                </Container>
+            </ div>
+        )
+    }
 }
 
 export default Accueil;
--- src/Footer.js
+++ src/Footer.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import { Nav, Navbar, Image, Container } from 'react-bootstrap';
+import { Image, Container, Row, Col } from 'react-bootstrap';
 
 function Footer() {
     return (
-        <Container fluid>
-            <footer>
-                <Navbar bg="light" variant="light" >
-                    <Nav className="mr-auto"></Nav>
-                    <Nav>
-                        <Nav.Item>
-                            <Nav.Link href="https://www.facebook.com/N%C3%A9m%C3%A9sis-1582053825444049/">
-                                <Image src="//logo.clearbit.com/facebook.com?size=28&greyscale=true" alt="facebook" roundedCircle></Image>
-                            </Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link href="https://twitter.com/NemesisproCM">
-                                <Image src="//logo.clearbit.com/twitter.com?size=28&greyscale=true" alt="twitter" roundedCircle></Image>
-                            </Nav.Link>
-                        </Nav.Item>
-                    </Nav>
-                </Navbar>
-            </footer>
+        <Container id="footer" fluid>
+            <Row>
+                <Col xs={11}></Col>
+                <Col xs={1} sm="auto">
+                    <a href="https://www.facebook.com/N%C3%A9m%C3%A9sis-1582053825444049/">
+                        <Image src="//logo.clearbit.com/facebook.com?size=28&greyscale=true" alt="facebook" roundedCircle></Image>
+                    </a>
+                    <a href="https://twitter.com/NemesisproCM">
+                        <Image src="//logo.clearbit.com/twitter.com?size=28&greyscale=true" alt="twitter" roundedCircle></Image>
+                    </a>
+                </Col>
+            </Row>
         </Container>
     )
 }
